67c07650f2d85144958d1b411519265de78f78ab
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-20 21:07:25 -0400

    connected mysql; added db.query

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const mysql = require("mysql2");
+
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,9 +10,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // MySQL username,
+        user: "root",
+        // MySQL Password
+        password: "@Springsidelane10",
+        database: "election"
+    },
+    console.log("Connected to the election database")
+);
 
+db.query("SELECT * FROM candidates", (err, rows) => {
+    console.log(rows);
+});
 
-//Default response for any other request (Not Found)
+//Catchall response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
