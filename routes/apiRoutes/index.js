c265bd61d9c7770c36b830d2ffaeb7ef87d6a4e2
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-22 21:33:39 -0400

    added voter GET POST PUT and DELETE routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.use(require("./candidateRoutes"));
-router.use(require("./partyRoutes"));
+router.use(require("./candidateRoutes.js"));
+router.use(require("./partyRoutes.js"));
+router.use(require("./voterRoutes.js"));
 
 module.exports = router;
