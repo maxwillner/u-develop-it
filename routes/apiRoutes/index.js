144fdcc62c473e2ce3eeae1748f0041833bab262
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-22 22:37:28 -0400

    added GET route to tally votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require("./candidateRoutes.js"));
 router.use(require("./partyRoutes.js"));
 router.use(require("./voterRoutes.js"));
+router.use(require("./voteRoutes.js"));
 
 module.exports = router;
